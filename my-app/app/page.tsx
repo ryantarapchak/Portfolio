8a7de51c29cd07f04d64fa2cc9b1eab90d5df521
ryantarapchak <ryantarapchak@gmail.com>
2026-03-30 03:16:39 +0000

    portfolio

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -298,7 +298,8 @@ export default function Home() {
             </h1>
 
             <p className="mt-2 text-base text-[#7CC4FA] sm:text-lg">
-              The Pennsylvania State University Master of Finance
+              The Pennsylvania State University 
+              Master of Finance
             </p>
 
             <p className="mt-1 text-sm text-slate-400">
